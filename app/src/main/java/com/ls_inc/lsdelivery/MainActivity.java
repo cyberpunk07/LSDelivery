c38c586e37a9b0b368204b1675f5ff795ecaefc2
cyburpunk07 <vinodcocmaster@gmail.com>
2019-09-24 17:37:24 +0530

    just a check

---
+++ app/src/main/java/com/ls_inc/lsdelivery/MainActivity.java
@@ -0,0 +1,14 @@
+   package com.ls_inc.lsdelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
